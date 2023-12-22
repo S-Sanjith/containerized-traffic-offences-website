7f2652cea2eb29e156390d8a49201af8059ade89
S Sanjith <83284162+S-Sanjith@users.noreply.github.com>
2023-12-22 05:46:27 +0000

    Modified

--- frontend/src/axios.js
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios'
-const instance= Axios.create({
-    baseURL: 'http://127.0.0.1:5000',
-    // baseURL: 'http://backend:5000',
-    
-
-})
-
-export default instance
--- frontend/src/random.js
+++
@@ -1,4 +0,0 @@
-const Random= ()=>{
-    return ('2022KAR'+Math.floor(Math.random()*100))
-}
-export default Random
